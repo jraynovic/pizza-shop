7bb2d31adb380f4b7afbbd89af1b41cbcf1f70eb
= <jraynovic@key.net>
2022-03-27 16:21:17 -0700

    build: options for order and logic to handle them

--- client/src/components.js/MainComponent.js
+++ client/src/components.js/MainComponent.js
@@ -2,16 +2,17 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import OrderComponent from "./OrderComponent";
 import OrdersComponent from "./OrdersComponent";
+import { Container } from "@mui/material";
 
 const MainComponent = () => {
   return (
-    <div>
+    <Container fluid>
       <Switch>
         <Route path="/order" component={OrderComponent} />
         <Route path="/orders" component={OrdersComponent} />
         <Redirect to="/order" />
       </Switch>
-    </div>
+    </Container >
   );
 };
 
--- client/src/components.js/OrderComponent.js
+++ client/src/components.js/OrderComponent.js
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  TextField,
+  Select,
+  Grid,
+  MenuItem,
+  FormControl,
+  FormControlLabel,
+  InputLabel,
+  Checkbox
 
+} from "@mui/material";
 const OrderComponent = () => {
-  return (
-    <div>OrderComponent</div>
-  )
-}
+  const [size, setSize] = useState("Large");
+  const [toppings, setToppings] = useState([]);
+  const [specialNotes, setSpecialNotes] = useState("");
+  const [orderSubmitted, setOrderSubmitted] = useState(false);
 
-export default OrderComponent
+  const sizeOptions = ["Large", "Medium", "Small"];
+  const toppingOptions = ["Pepperoni", "Sausage", "Mushrooms", "Bell Pepper"];
+
+  const handleToppings = (topping)=>{
+    console.log(typeof(toppings))
+    if(toppings.indexOf(topping) !== -1){
+      console.log(`${topping} is already added`)
+      setToppings(toppings.filter(topp=> topp !== topping))
+    } 
+    else{
+      
+      setToppings([...toppings, topping])
+    } 
+    console.log(toppings)
+
+  }
+
+  const renderToppings = ()=>{
+    // return toppingOptions.map(topping=> <Grid item md={6}><FormControlLabel>{topping}</FormControlLabel>AHHH</Grid>)
+    return(
+      toppingOptions.map((topping, index)=>{
+        return (
+          <Grid key={topping+index} md={12} style={{ marginTop: 20 }}>
+            <FormControlLabel control={<Checkbox id={topping} onChange={(e,val,label)=>handleToppings(e.target.id)} checked={toppings.includes(topping)}/>} label={topping} />
+          </Grid>
+        );
+      })
+    )
+  }
+
+  return (
+    <div>
+      <Grid container>
+        <Grid item xs={12}>
+          {" "}
+          <h2>Pizza!</h2>
+        </Grid>
+        <Grid item xs={12} md={6} >
+          <Grid item xs={12} style={{ marginTop: 9,textAlign:'left'}}>
+            <FormControl>
+              <InputLabel>Size</InputLabel>
+              <Select
+                fullWidth
+                label={"Size"}
+                defaultValue={"Large"}
+                onChange={(e, value) => setSize(value)}
+              >
+                {sizeOptions.map((sizeOp) => (
+                  <MenuItem value={sizeOp}>{sizeOp}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            {renderToppings()}
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          Your order...
+          <Grid item xs={12}>{size}</Grid>
+          <Grid item xs={12}>{toppings.map(topping=><div>{topping}</div>)}</Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default OrderComponent;
